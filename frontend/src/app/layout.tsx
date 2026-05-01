d8ac11d067417fb3c0d4001dbdf72bd33cfd3b33
Rohit Biruli <your-email@gmail.com>
2026-05-01 18:40:18 +0530

    Installed Taboola Pixel and applied build fixes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -37,6 +37,22 @@ export default function RootLayout({
             window.adBreak = function(o) {adsbygoogle.push(o);};
           `}
         </Script>
+        
+        {/* Taboola Pixel Code */}
+        <Script id="taboola-pixel" strategy="afterInteractive">
+          {`
+            window._tfa = window._tfa || [];
+            window._tfa.push({notify: 'event', name: 'page_view', id: 1888730});
+            !function (t, f, a, x) {
+              if (!document.getElementById(x)) {
+                t.async = 1; t.src = a; t.id=x; f.parentNode.insertBefore(t, f);
+              }
+            }(document.createElement('script'),
+            document.getElementsByTagName('script')[0],
+            '//cdn.taboola.com/libtrc/unip/1888730/tfa.js',
+            'tb_tfa_script');
+          `}
+        </Script>
         <Providers>
           <div className="flex flex-col min-h-screen">
             <Header />
